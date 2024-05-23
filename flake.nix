c4dcae697b143c9d9c5a87b2d11acf2a19f65d9b
treo <treo@treo.com>
2024-05-23 18:30:57 -0500

    add home-manager

--- flake.nix
+++ flake.nix
@@ -1,12 +1,41 @@
 {
   description = "A NixOS configuration flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.NickeyMouseOS = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager }: 
+  let 
       system = "x86_64-linux";
+  in {
+
+# =================================
+# nixos-configuration
+# =================================
+    nixosConfigurations.NickeyMouseOS = nixpkgs.lib.nixosSystem {
       modules = [ ./configuration.nix ];
+    };
+
+
+
+# =================================
+# home-manager
+# =================================
+    homeConfigurations = {
+      myHome = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+	  inherit system;
+          config.allowUnfree = true; # プロプライエタリなパッケージを許可
+        };
+        modules = [
+          ./home.nix
+        ];
+      };
     };
   };
 }
