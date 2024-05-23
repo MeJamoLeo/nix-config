117290dcd27b399ddb9f01230de885a60f91a63f
treo <treo@treo.com>
2024-05-23 12:04:35 -0500

    add softwares

--- configuration.nix
+++ configuration.nix
@@ -95,11 +95,14 @@
 
   # Install firefox.
   programs.firefox.enable = true;
+  programs.git.enable = true;
+  programs.neovim.enable = true;
+  programs.starship.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      git
      xclip
   #  wget
