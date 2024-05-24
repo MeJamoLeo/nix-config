df9bbd4a0025677cac292ac180ca75cbcde1db6a
treo <treo@treo.com>
2024-05-24 16:35:00 -0500

    i3

--- home.nix
+++ home.nix
@@ -16,4 +16,17 @@
 
   programs.neovim.enable = true;
   programs.git.enable = true;
+
+
+  xsession.windowManager.i3 = {
+	  enable = true;
+	  package = pkgs.i3-gaps;
+	  config = {
+		  modifier = "Mod4";
+		  gaps = {
+			  inner = 10;
+			  outer = 5;
+		  };
+	  };
+  };
 }
