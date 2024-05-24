53ba59797cc47e31fe8d73087db2ae436da94005
treo <treo@treo.com>
2024-05-24 16:35:32 -0500

    i3

--- configuration.nix
+++ configuration.nix
@@ -84,6 +84,7 @@
   # Enable the XFCE Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
+  services.xserver.windowManager.i3.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
