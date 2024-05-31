51f7844a0ba616c539fb2061b066b0cd4ab63ffc
treo <treo@treo.com>
2024-05-31 17:10:01 -0500

    add nvim lsp plugin & format

--- home.nix
+++ home.nix
@@ -1,44 +1,54 @@
 {pkgs, ...}:
 {
   home = rec { # recでAttribute Set内で他の値を参照できるようにする
-    username="bbb";
-    homeDirectory = "/home/${username}"; # 文字列に値を埋め込む
-    stateVersion = "22.11";
+  username="bbb";
+  homeDirectory = "/home/${username}"; # 文字列に値を埋め込む
+  stateVersion = "22.11";
+};
+programs.home-manager.enable = true; # home-manager自身でhome-managerを有効化
+
+home.packages = with pkgs; [
+  bat
+  eza
+  ripgrep
+  lazygit
+  online-judge-tools
+
+  nodejs_22
+  python3
+  sbcl #common lisp compiler
+];
+
+programs.neovim = {
+  enable = true;
+  plugins = with pkgs.vimPlugins; [
+    vim-nix
+    {
+      plugin = iceberg-vim;
+      config = ''
+         colorscheme iceberg
+      '';
+    }
+    {
+      plugin = coc-nvim;
+      config = ''
+         let g:coc_global_extensions = [ 'coc-nix', 'coc-clangd' ]
+      '';
+    }
+  ];
+};
+
+programs.git.enable = true;
+
+xsession.windowManager.i3 = {
+  enable = true;
+  package = pkgs.i3-gaps;
+  config = {
+    modifier = "Mod4";
+    gaps = {
+      inner = 10;
+      outer = 5;
+    };
   };
-  programs.home-manager.enable = true; # home-manager自身でhome-managerを有効化
-
-   home.packages = with pkgs; [
-   bat
-   eza
-   ripgrep
-   lazygit
-   online-judge-tools
- ];
-
-  programs.neovim = {
-	  enable = true;
-	  plugins = with pkgs.vimPlugins; [
-		  vim-nix
-		  {
-			  plugin = iceberg-vim;
-			  config = ''
-				  colorscheme iceberg
-				  '';
-		  }
-	  ];
-  };
-
-  programs.git.enable = true;
-
-  xsession.windowManager.i3 = {
-	  enable = true;
-	  package = pkgs.i3-gaps;
-	  config = {
-		  modifier = "Mod4";
-		  gaps = {
-			  inner = 10;
-			  outer = 5;
-		  };
-	  };
-  };
+};
 }
