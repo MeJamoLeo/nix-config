275affaf7619ed25f97ed14e6b445ce7192852fe
treo <treo@treo.treo>
2024-06-12 15:59:57 -0500

    format

--- home.nix
+++ home.nix
@@ -6,118 +6,88 @@
   ];
 
   home = rec { # recでAttribute Set内で他の値を参照できるようにする
-  username="bbb";
+  username="treo";
   homeDirectory = "/home/${username}"; # 文字列に値を埋め込む
   stateVersion = "22.11";
 };
+
 programs.home-manager.enable = true; # home-manager自身でhome-managerを有効化
 
+# home.nix
 home.packages = with pkgs; [
   #=============
   # GUI
   #=============
-  bat
-  eza
-  ripgrep
-  lazygit
-  online-judge-tools
+    bat
+    btop
+    eza
+    ripgrep
+    lazygit
+    online-judge-tools
 
-  nodejs_22
-  python3
-  sbcl #common lisp compiler
+    nodejs_22
+    python3
+    sbcl #common lisp compiler
 
   #=============
   # GUI
   #=============
-  discord
-  obsidian
-  virtualbox
-];
-
-programs.neovim = {
-  enable = true;
-  plugins = with pkgs.vimPlugins; [
-    vim-nix
-    {
-      plugin = iceberg-vim;
-      config = ''
-        colorscheme iceberg
-      '';
-    }
-    {
-      plugin = coc-nvim;
-      config = ''
-        let g:coc_global_extensions = [ 'coc-nix', 'coc-clangd' ]
-
-        highlight CocErrorSign ctermfg=15 ctermbg=196
-        highlight CocWarningSign ctermfg=0 ctermbg=172
-
-        "select with Enter
-        inoremap <expr> <cr> pumvisible() ? '<c-y>' : '<cr>'
-      '';
-    }
-    {
-      plugin = rainbow;
-      config = ''
-        let g:rainbow_active = 1 "set to 0 if you want to enable it later via :RainbowToggle
-      '';
-    }
+    brave
+    discord
+    obsidian
+    virtualbox
+    vivaldi
+    vscode
   ];
-};
 
-programs.git.enable = true;
+  programs.neovim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      vim-nix
+      {
+        plugin = iceberg-vim;
+        config = ''
+          colorscheme iceberg
+        '';
+      }
+      {
+        plugin = coc-nvim;
+        config = ''
+          let g:coc_global_extensions = [ 'coc-nix', 'coc-clangd' ]
 
-services.picom = {
-  activeOpacity = 0.95;
-  backend = "glx";
-  enable = true;
-# extraArgs
-fade = true;
-# fadeDelta
-# fadeExclude
-fadeSteps = [0.1 0.03];
-inactiveOpacity = 0.9;
-menuOpacity = 0.8;
-opacityRules = [
-  "100:class_g = 'zoom'"
-  "100:class_g = 'Brave-browser'"
-  "100:class_g = 'Firefox'"
-  "100:class_g = 'konqueror'"
-  "100:window_type = 'dock' && class_g = 'i3bar'"
-  "80:window_type = 'dock'"
-];
-# package
-# settings #Picom settings. Use this option to configure Picom settings not exposed in a NixOS option or to bypass one. For the available options see the CONFIGURATION FILES section at picom(1).
-# shadow
-shadowExclude = 
-[
-  "name = 'Notification'"
-  "class_g = 'Conky'"
-  "class_g ?= 'Notify-osd'"
-  "class_g = 'Cairo-clock'"
-  "_GTK_FRAME_EXTENTS@:c"
-];
-# shadowOffsets
-# shadowOpacity
-vSync = true;
-# wintypes = 
-# {
-#   tooltip = { fade = true; shadow = false; opacity = 0.9; focus = true; full-shadow = false; }
-#   dock = { shadow = false; }
-#   dnd = { shadow = false; }
-#   popup_menu = { opacity = 0.9; }
-#   dropdown_menu = { opacity = 0.9; }
-# };
-};
+          highlight CocErrorSign ctermfg=15 ctermbg=196
+          highlight CocWarningSign ctermfg=0 ctermbg=172
 
+          "select with Enter
+          inoremap <expr> <cr> pumvisible() ? '<c-y>' : '<cr>'
+        '';
+      }
+      {
+        plugin = rainbow;
+        config = ''
+          let g:rainbow_active = 1 "set to 0 if you want to enable it later via :RainbowToggle
+        '';
+      }
+    ];
+  };
 
-programs.alacritty = {
-  enable = true;
+  programs.kitty = {
+    enable = true;
   # package
   # settings
 };
 
 programs.qutebrowser = {
   enable = true;
+  extraConfig = ''
+  config.set('content.javascript.clipboard', 'access', 'github.com')
+  config.set('content.javascript.clipboard', 'access', 'atcoder.jp')
+  config.set('content.javascript.clipboard', 'access', 'chat.openai.com')
+  '';
 };
+
+programs.starship = {
+  enable = true;
+};
+
 }
