2fae00f96296e56006a154fd302ad598a958bcc5
treo <treo@treo.treo>
2024-06-13 17:53:52 -0500

    set default editor

--- home.nix
+++ home.nix
@@ -9,6 +9,10 @@
   username="treo";
   homeDirectory = "/home/${username}"; # 文字列に値を埋め込む
   stateVersion = "22.11";
+
+  sessionVariables = {
+    EDITOR = "nvim";
+  };
 };
 
 programs.home-manager.enable = true; # home-manager自身でhome-managerを有効化
