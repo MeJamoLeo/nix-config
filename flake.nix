5607deb18978fd69b6981a18617c4bdaac6f426f
treo <treo@treo.treo>
2024-06-13 19:01:18 -0500

    edit flake.nix

--- flake.nix
+++ flake.nix
@@ -10,6 +10,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    hyprland = {
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager }: 
@@ -37,6 +41,8 @@
       };
       modules = [
         ./home.nix
+        hyprland.homeManagerModules.default
+        {wayland.windowManager.hyprland.enable = true;}
       ];
     };
   };
