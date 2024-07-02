ae10b6f4966fae758260fbe40b9d148c43f14996
treo <treo@treo.treo>
2024-07-02 04:14:16 -0500

    format

--- home.nix
+++ home.nix
@@ -220,46 +220,46 @@ home.packages = with pkgs; [
     settings = {
       font_size = "10";
     };
-};
+  };
 
-programs.qutebrowser = {
-  enable = true;
-  extraConfig = ''
-    config.set('content.javascript.clipboard', 'access', 'github.com')
-    config.set('content.javascript.clipboard', 'access', 'atcoder.jp')
-    config.set('content.javascript.clipboard', 'access', 'chat.openai.com')
+  programs.qutebrowser = {
+    enable = true;
+    extraConfig = ''
+      config.set('content.javascript.clipboard', 'access', 'github.com')
+      config.set('content.javascript.clipboard', 'access', 'atcoder.jp')
+      config.set('content.javascript.clipboard', 'access', 'chat.openai.com')
 
-    config.set('content.blocking.enabled', True)
+      config.set('content.blocking.enabled', True)
 
 
     # TODO: I cannot use 'j' for input
     # config.bind('jj', 'mode-leave', mode='insert')
 
     # Unbind the original keybindings
-    config.unbind('<Shift-j>', mode='normal')
-    config.unbind('<Shift-k>', mode='normal')
+      config.unbind('<Shift-j>', mode='normal')
+      config.unbind('<Shift-k>', mode='normal')
 
-    config.bind('<Shift-j>', 'tab-prev', mode='normal') # Bind Shift+j to move the tab focus to the left
-    config.bind('<Shift-k>', 'tab-next', mode='normal') # Bind Shift+k to move the tab focus to the right
-  '';
-  searchEngines = {
-    "DEFAULT" = "https://duckduckgo.com/?ia=web&q={}";
-    "!g" = "https://www.google.com/search?hl=en&q={}";
-    "!gm" = "https://www.google.com/maps?q={}";
-    "!d" = "https://duckduckgo.com/?ia=web&q={}";
-    "!nw" = "https://nixos.wiki/index.php?search={}";
-    "!np" = "https://github.com/NixOS/nixpkgs/search?q={}";
-    "!no" = "https://mynixos.com/search?q={}";
-    "!hp" = "https://github.com/nix-community/home-manager/search?q={}";
-    "!a" = "https://www.amazon.com/s?k={}";
-    "!gh" = "https://github.com/search?o=desc&q={}&s=stars";
-    "!ghn" = "https://github.com/search?o=desc&q=lang%3ANix+{}&type=code";
-    "!s" = "https://sourcegraph.com/search?patternType=standard&sm=1&q=context:global+{}";
-    "!y" = "https://www.youtube.com/results?search_query={}";
-  };
-  settings = {
-    fonts.default_size = "10pt";
-    zoom.default = "75%"; # for laptop
+      config.bind('<Shift-j>', 'tab-prev', mode='normal') # Bind Shift+j to move the tab focus to the left
+      config.bind('<Shift-k>', 'tab-next', mode='normal') # Bind Shift+k to move the tab focus to the right
+    '';
+    searchEngines = {
+      "DEFAULT" = "https://duckduckgo.com/?ia=web&q={}";
+      "!g" = "https://www.google.com/search?hl=en&q={}";
+      "!gm" = "https://www.google.com/maps?q={}";
+      "!d" = "https://duckduckgo.com/?ia=web&q={}";
+      "!nw" = "https://nixos.wiki/index.php?search={}";
+      "!np" = "https://github.com/NixOS/nixpkgs/search?q={}";
+      "!no" = "https://mynixos.com/search?q={}";
+      "!hp" = "https://github.com/nix-community/home-manager/search?q={}";
+      "!a" = "https://www.amazon.com/s?k={}";
+      "!gh" = "https://github.com/search?o=desc&q={}&s=stars";
+      "!ghn" = "https://github.com/search?o=desc&q=lang%3ANix+{}&type=code";
+      "!s" = "https://sourcegraph.com/search?patternType=standard&sm=1&q=context:global+{}";
+      "!y" = "https://www.youtube.com/results?search_query={}";
+      };
+      settings = {
+      fonts.default_size = "10pt";
+      zoom.default = "75%"; # for laptop
     # zoom.default = "100%"; # for 4k monitor
   };
 };
