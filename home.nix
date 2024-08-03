d94dc34a872597d15be020a6a6311c05857620c2
treo <treo@treo.treo>
2024-08-03 03:38:39 -0500

    update

--- home.nix
+++ home.nix
@@ -7,7 +7,7 @@
   home = rec { # recでAttribute Set内で他の値を参照できるようにする
   username="treo";
   homeDirectory = "/home/${username}"; # 文字列に値を埋め込む
-  stateVersion = "22.11";
+  stateVersion = "23.11";
 };
 
 programs.home-manager.enable = true; # home-manager自身でhome-managerを有効化
